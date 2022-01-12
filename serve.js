9c857fad11398c91ce1da2d581b103827dd9e4eb
tanktejas <tejastank20001@gmail.com>
2022-01-12 17:22:33 +0530

    a

--- serve.js
+++ serve.js
@@ -7,49 +7,56 @@ app.use(express.urlencoded({extended:false}))
 // this we have to write when our local host deny to connect with another local host.
 const cors=require("cors");
 const corsOptions ={
-  origin:'*', 
+  origin:'*',   
   credentials:true,            
   //  access-control-allow-credentials:true,
   optionSuccessStatus:200,
 }
- 
 
 app.use(cors(corsOptions));
 
-mongoose.connect("mongodb+srv://tejas:ab@cluster0.bczol.mongodb.net/test",{useNewUrlParser: true, 
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://tejas:ab@cluster0.bczol.mongodb.net/test",{useNewUrlParser: true, 
 useUnifiedTopology: true
 }).then(()=>{  
   console.log("success!!");    
 });
 
 const schema=new mongoose.Schema({       
-  name:String
+  name:String,
+  comment:String  
 });   
 
+  
 const mod=new mongoose.model("Blog",schema);
 
 const b1=new mod({    
   name:"tejas"      
 })     
 
-b1.save(); 
+// b1.save(); 
 
 app.use(express.static(path.join(__dirname,'build')));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname + 'build' , 'index.html'));
-})
-   
+  console.log("as");
+    res.sendFile(path.join(__dirname + 'build', 'index.html'));
+})   
+
 app.use(express.json());
-
-
-app.post('/datarec',(req,res)=>{
+app.post('/adddata',(req,res)=>{
+  console.log("as");
      let data=req.body;
       let val=new mod(data);
-      mod.save();
-
+      val.save();
+      
 })
   
-app.listen(process.env.PORT || 3004,()=>{
+app.post('/datastore',async (req,res)=>{  
+  let data= await mod.find();
+  res.send(data);    
+});
+  
+    
+app.listen(process.env.PORT || 3001,()=>{
   console.log("success!!");
 })
