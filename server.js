4e74ecade03c1e185d9308cbd34024f806d8be12
tejas-tank <tejasget2learn@gmail.com>
2022-04-11 23:37:25 +0530

    add

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ const data = new referral({
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "build", "index.html"));
+  res.send("Hello home page !!");
 });
 
 app.use(express.json());
